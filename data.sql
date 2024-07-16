7cebe5fd190d679bc225315b9fdb5ea0835ab184
Henintsoa Paul <henintsoapaul@gmail.com>
2024-07-16 17:34:59 +0300

    data

--- data.sql
+++ data.sql
@@ -4,3 +4,12 @@ INSERT INTO garage_client values (default, '6090TBB', 1), (default, '7890TAB', 1
 
 INSERT INTO garage_slot values
 (default, 'A'), (default, 'B'), (default, 'C');
+
+INSERT INTO garage_date_reference VALUES
+(default, '2024-01-01');
+
+INSERT INTO garage_admin VALUES
+(default, 'asta@gmail.com', 'atlasss');
+
+INSERT INTO garage_heure_travail VALUES
+(default, '08:00:00', '18:00:00');
