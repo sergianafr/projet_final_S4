a77ec4bda3831df355d123e97b04d012895fabb3
ChanAlex2357 <jacqueschanalex@gmail.com>
2024-07-16 17:35:25 +0300

    script update

--- data.sql
+++ data.sql
@@ -4,3 +4,10 @@ INSERT INTO garage_client values (default, '6090TBB', 1), (default, '7890TAB', 1
 
 INSERT INTO garage_slot values
 (default, 'A'), (default, 'B'), (default, 'C');
+
+INSERT INTO garage_admin VALUES
+(default, 'asta@gmail.com', 'atlasss');
+
+INSERT INTO garage_heure_travail VALUES
+(default, '08:00:00', '18:00:00');
+
