16b14222b764349be713b1b45cf3133f52d44bce
Henintsoa Paul <henintsoapaul@gmail.com>
2024-07-16 05:54:19 +0300

    Update fonction slot

--- Fonction.sql
+++ Fonction.sql
@@ -5,13 +5,13 @@ CREATE PROCEDURE slots_disponibles(
     IN dateheurefin_donnee DATETIME
 )
 BEGIN
-    SELECT s.id, s.libelle
+    SELECT DISTINCT s.id, s.libelle
         FROM slot s
         LEFT JOIN details_rdv dr ON s.id = dr.idSlot
         WHERE NOT (
             (dateheuredebut_donnee BETWEEN dr.date_heure_debut AND dr.date_heure_fin)
             OR (dateheurefin_donnee BETWEEN dr.date_heure_debut AND dr.date_heure_fin)
-        ) AND (dateheuredebut_donnee <  dr.date_heure_debut AND dateheurefin_donnee >  dr.date_heure_fin )
+        ) AND (dateheuredebut_donnee >=  dr.date_heure_debut AND dateheurefin_donnee >  dr.date_heure_fin )
         OR dr.id IS NULL;
 END //
 
