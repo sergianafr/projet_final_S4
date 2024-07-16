fcc04fb65b39f2affc640099e6d1382b8be6795b
Henintsoa Paul <henintsoapaul@gmail.com>
2024-07-16 15:18:22 +0300

    Import service from csv

--- application/models/services_temp_model.php
+++ application/models/services_temp_model.php
@@ -1,32 +1,35 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-class services_temp_model extends CI_Model {
-    public function __construct(){
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class services_temp_model extends CI_Model
+{
+    public function __construct()
+    {
         parent::__construct();
         $this->load->helper('csvimport');
         $this->load->model('CRUD_model');
     }
 
-    public function save_to_temp($filename){
+    public function save_to_temp($filename)
+    {
         // Step 1: Clear existing data
-           
         $this->db->empty_table('services_temp');
 
         if (!is_readable($filename)) {
             throw new Exception("File not readable: " . $filename);
         }
 
+
         // Step 2: Insert data from csv into temp table
-         // Adjust the file path accordinglyecho "midiiiiitra";
-       
+        // Adjust the file path accordinglyecho "midiiiiitra";
         if (($handle = fopen($filename, 'r')) !== FALSE) {
 
-            if (($header = fgetcsv($handle, ',')) === FALSE) {
+            if (($header = fgetcsv($handle, 1000000, ',')) === FALSE) {
                 throw new Exception("Failed to read the header row from the CSV file.");
             }
 
-            
-            while (($data = fgetcsv($handle,',')) !== FALSE) {
+
+            while (($data = fgetcsv($handle, 1000000, ',')) !== FALSE) {
                 if (count($data) == 2) {  // Ensure correct number of columns
                     $serv_data = array(
                         'service' => $data[0],  // Adjust the indexes according to the structure of your csv file
@@ -38,9 +41,4 @@ class services_temp_model extends CI_Model {
         }
         fclose($handle);
     }
-
-   
-    
-
-   
 }
