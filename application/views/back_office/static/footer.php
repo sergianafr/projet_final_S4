b65fbfc7cb9ce3b14fa703c4f703fdd80297c66d
Henintsoa Paul <henintsoapaul@gmail.com>
2024-07-16 17:14:30 +0300

    script sql

---
+++ application/views/back_office/static/footer.php
@@ -0,0 +1,30 @@
+<footer class=" bg-light">
+                <div class="container">
+                    <div class="row">
+                        <div class=" col text-dark">
+                            <h2>Contributors</h2>
+                            <ul>
+                                <li>ETU002443</li>
+                                <li>ETU002434</li>
+                                <li>ETU002610</li>
+                            </ul>
+                        </div>
+                        <div class="col text-dark">
+                            
+                        </div>
+                        <div class="col text-dark">
+                            <h2>Links</h2>
+                            <ul>
+                                <li>Google Sheet</li>
+                                <li><a href="<?= site_url('script') ?>">Script Sql</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div><!-- end wrap -->
+        <script src="<?=base_url('assets/js/bootstrap.bundle.min.js')?>"></script>
+        <script src="<?=base_url('assets/js/calendar.min.js')?>"></script>
+        <script src="<?=base_url('assets/js/chart.js')?>"></script>
+    </body>
+</html>
