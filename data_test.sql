76f44aad0ca09b5d1ba2149341757fc439f090f8
Henintsoa Paul <henintsoapaul@gmail.com>
2024-07-16 16:32:15 +0300

    Montant rdv

--- data_test.sql
+++ data_test.sql
@@ -4,13 +4,21 @@ INSERT INTO client values (default, '6090TBB', 1), (default, '7890TAB', 1), (def
 
 INSERT INTO slot values
 (default, 'A'), (default, 'B'), (default, 'C');
-INSERT INTO date_reference VALUES(null, '2024-07-16');
 
--- INSERT INTO type_service values 
--- (default, 'Reparation simple', '1:00:00', 150000),
--- (default, 'Reparation standard', '2:00:00', 250000),
--- (default, 'Reparation complexe', '8:00:00', 800000),
--- (default, 'Entretien', '2:30:00', 300000);
+INSERT INTO date_reference VALUES
+(default, '2024-01-01');
+
+INSERT INTO admin VALUES
+(default, 'asta@gmail.com', 'atlasss');
+
+INSERT INTO heure_travail VALUES
+(default, '08:00:00', '18:00:00');
+
+INSERT INTO type_service values 
+(default, 'Reparation simple', '1:00:00', 150000),
+(default, 'Reparation standard', '2:00:00', 250000),
+(default, 'Reparation complexe', '8:00:00', 800000),
+(default, 'Entretien', '2:30:00', 300000);
 
 INSERT INTO rendez_vous VALUES
 (default, 1, '2024-07-07 8:00:00', '2024-07-02', 2, 1,  150000, default);
@@ -62,12 +70,3 @@ INSERT INTO rendez_vous VALUES
 (default, 1, '2024-07-19 8:00:00', '2024-07-19', 2, 3,  150000, default);
 INSERT INTO details_rdv VALUES
 (default, 8, '2024-07-19 8:00:00', '2024-07-19 10:00:00', 3, '2:00:00');
-
-INSERT INTO date_reference VALUES
-(default, '2024-01-01');
-
-INSERT INTO admin VALUES
-(default, 'asta@gmail.com', 'atlasss');
-
-INSERT INTO heure_travail VALUES
-(default, '08:00:00', '18:00:00');
