7478161516fd700b929088f1c1c7f2061934fb13
ChanAlex2357 <jacqueschanalex@gmail.com>
2024-07-16 17:22:19 +0300

    ensemble

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/CodeIgniter/';
+
+$config['base_url'] = '/ETU00234/t/projet_final_s4/';
 
 /*
 |--------------------------------------------------------------------------
