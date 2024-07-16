67260df5ba25561105d953571e98e97fc99c5ce1
Henintsoa Paul <henintsoapaul@gmail.com>
2024-07-16 09:38:29 +0300

    Redirection modify service

--- application/controllers/service.php
+++ application/controllers/service.php
@@ -1,19 +1,31 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class service extends CI_Controller {
+class service extends CI_Controller
+{
+    public $ts;
+    public $input;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('type_service_model', 'ts');
+    }
+
     /**
-	 * Redirection vers la page d'insertion
-	 */
-    function formulaire(){
+     * Redirection vers la page d'insertion
+     */
+    function formulaire()
+    {
         $data['service'] = null;
         $data['contents'] = "back_office/service/formulaire_service";
-		$this->load->view('templates/back_office_template',$data);
+        $this->load->view('templates/back_office_template', $data);
     }
     /**
      * Insertion 
      */
-    function insertion(){
+    function insertion()
+    {
         // Recuperation des donneess
 
         // Insertion
@@ -21,36 +33,38 @@ class service extends CI_Controller {
         // Redirection vers la liste des services
         redirect('back_office/service');
     }
+
     /**
      * Redirection vers la page d'insertion avec le service a modifier
      */
-    function modifier() {
-        // Recuperation de l'id par l'url
+    function modifier()
+    {
+        $id_service = intval($this->input->get('id_service'));
 
-        // Recuperation du service par l'id
-        $service = "null";
+        $service = $this->ts->get_by_id($id_service);
+
         // Afficher le formulaire preremplie
         $data['service'] = $service;
         $data['contents'] = "back_office/service/formulaire_service";
-        $this->load->view('templates/back_office_template',$data);
+        $this->load->view('templates/back_office_template', $data);
     }
     /**
      * Modification d'un service
      */
-    function modification() {
+    function modification()
+    {
         // Recuperation des informations du service
-        
+
         // Redirection vers la liste des services
-		redirect('back_office/service');
-	}
-    function supprimer() {
+        redirect('back_office/service');
+    }
+    function supprimer()
+    {
         // Recuperation de l'id par l'url
 
         // Suppression du service
 
         // Redirection vers la liste des services
         redirect('back_office/service');
-
     }
 }
-?>
--- application/views/back_office/service/formulaire_service.php
+++ application/views/back_office/service/formulaire_service.php
@@ -1,40 +1,41 @@
-<?php 
+<?php
 
 $id_service = 0;
 $nom_service = "";
 $prix_service = 0;
 $duree_service = 1;
 $action = "insertion";
-$btn_text = "Enregistrer";
+$btn_text = "Ajouter";
 
 if ($service != null) {
     $action = "modification";
     $btn_text = "Modifier";
-    // TODO : Attribution des attribut du service
 
+    $nom_service = $service['libelle'];
+    $prix_service = $service['prix'];
+    $duree_service = $service['duree'];
 }
 ?>
 
-
 <section class="container">
-    <h1>Formulaire de service</h1>
+    <h1><?= $btn_text ?> service</h1>
     <div class="row">
         <div class="col w-50 mx-auto">
-            <form method="post" action="<?=site_url("back_office/service/$action")?>">
+            <form method="post" action="<?= site_url("back_office/service/$action") ?>">
                 <div class="form-floating mb-3">
-                    <input type="text" name="nom" id="nomInput" class="form-control" value="<?=$nom_service?>">
+                    <input type="text" name="nom" id="nomInput" class="form-control" value="<?= $nom_service ?>">
                     <label for="nomInput">Nom service</label>
                 </div>
                 <div class="form-floating mb-3">
-                    <input type="time" name="durre" id="dureeInput" class="form-control" value="<?=$duree_service?>">
+                    <input type="time" name="durre" id="dureeInput" class="form-control" value="<?= $duree_service ?>">
                     <label for="dureeInput">Duree (h)</label>
                 </div>
                 <div class="form-floating mb-3">
-                    <input type="number" min="0.0" step="0.1" name="prix" id="prixInput" class="form-control">
+                    <input type="number" min="0.0" step="0.1" name="prix" id="prixInput" class="form-control" value="<?= $prix_service ?>">
                     <label for="prixInput">Prix</label>
                 </div>
                 <div class="mb-3">
-                    <input class="btn btn-primary" type="submit" value="<?=$btn_text?>">
+                    <input class="btn btn-primary" type="submit" value="<?= $btn_text ?>">
                 </div>
             </form>
         </div>
